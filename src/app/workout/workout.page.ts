41f61657af878bb980aa87de801b08b4137b7b46
robert lisle <robbie.lisle@live.com>
2019-06-25 16:59:35 +0100

    workout page template

---
+++ src/app/workout/workout.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workout',
+  templateUrl: './workout.page.html',
+  styleUrls: ['./workout.page.scss'],
+})
+export class WorkoutPage implements OnInit {
+
+  public items: Array<{ title: string; note: string;}> = [];
+  constructor() {
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i
+      });
+    }
+  }
+
+  ngOnInit() {
+  }
+}
